c173596d347bf586977c81d3db002cc42241766c
MCCSharp <c.mikePro@outlook.com>
2024-05-06 12:06:56 +0200

    Sion Projket

---
+++ Js/styles.js
@@ -0,0 +1,33 @@
+// function MenuHamburger(){
+//     const menuHamburger = document.querySelector(".menu-hamburger") //menu-hamburger is classname
+//     const navLinks = document.querySelector(".nav-link")
+
+//     menuHamburger.addEventListener('click', ()=>{
+//         navLinks.classList.toggle('mobile-menu')
+//     })
+// }
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     const collapseButton = documemnt.querySelector(".collapse-btn");
+//     const collapseContent= document.querySelector(".collapse-ctn");
+
+//     toggleButton.addEventListener("click", function(){
+//         if(collapseContent.style.display === "none"){
+//             collapseContent.style.display = "block";
+//             collapseButton.textContent = "- Weniger Anzeigen";
+//         }else{
+//             collapseContent.style.display="none";
+//             collapseButton.textContent="+ Mehr Anzeigen";
+//         }
+//     });
+// });
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+// });
+
+
+
+
